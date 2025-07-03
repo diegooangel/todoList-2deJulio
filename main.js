017b975b749679e48b9c59b8dc763c38cd5fca3a
diegooangel <diego09.11.2000@gmail.com>
2025-07-03 00:22:43 -0300

    Todo list basico, solo se puede agregar tareas.

---
+++ main.js
@@ -0,0 +1,25 @@
+//Con esto hacemos que al apretar el boton no se recargue la pagina automaticamente.
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+//Almacenar el valor del input:
+const input = document.getElementById('input');
+
+//Almacenar en una variable un "ul" para poder listar las tareas con un "li".
+const ul = document.getElementById('ul');
+
+//Funcion donde se dispare un evento cuando se activa el boton.
+const listar = () => {
+    const li = document.createElement('li');
+    const contenido = document.createTextNode(input.value);
+    ul.appendChild(li);
+    li.appendChild(contenido);
+
+    input.value = "";
+}
+
+
+
+
