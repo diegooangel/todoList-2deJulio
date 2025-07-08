1234a35576d11cacdf46fef313d7ff91fe602cd9
diegooangel <diego09.11.2000@gmail.com>
2025-07-08 00:00:37 -0300

    creamos la funcionalidad para eliminar la tarea realizada

--- main.js
+++ main.js
@@ -7,24 +7,28 @@ form.addEventListener('submit', (e) => {
 //Almacenar el valor del input:
 const input = document.getElementById('input');
 
-//Almacenar en una variable un "ul" para poder listar las tareas con un "li".
-const ul = document.getElementById('ul');
-
 //Funcion donde se dispare un evento cuando se activa el boton.
 const listar = () => {
+//Almacenar en una variable un "ul" para poder listar las tareas con un "li".
+    const ul = document.getElementById('ul');
     const li = document.createElement('li');
     const contenido = document.createTextNode(input.value);
     ul.appendChild(li);
     li.appendChild(contenido);
 
-//Creamos el boton para eliminar la tarea.
+    //Creamos el boton para eliminar la tarea.
     const boton = document.createElement('button');
     const contenidoBoton = document.createTextNode('Borrar');
     li.appendChild(boton);
     boton.appendChild(contenidoBoton);
 
+    boton.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    });
+
+
     input.value = "";
 }
-
 
 
