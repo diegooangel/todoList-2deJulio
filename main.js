10e67ad39a3d20ad2a53d89b88a3d24aa67391a8
diegooangel <diego09.11.2000@gmail.com>
2025-07-14 00:22:11 -0300

    Borre la otra forma de listar una tarea. Cree una forma mas facil para poder usar los botones

--- main.js
+++ main.js
@@ -4,41 +4,30 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 
-//Almacenar el valor del input:
 const input = document.getElementById('input');
-
-//Funcion donde se dispare un evento cuando se activa el boton.
-const listar = () => {
-//Almacenar en una variable un "ul" para poder listar las tareas con un "li".
-    const ul = document.getElementById('ul');
-    const li = document.createElement('li');
-    const contenido = document.createTextNode(input.value);
-    ul.appendChild(li);
-    li.appendChild(contenido);
-
-    //Creamos el boton para eliminar la tarea.
-    const boton = document.createElement('button');
-    const contenidoBoton = document.createTextNode('Borrar');
-    li.appendChild(boton);
-    boton.appendChild(contenidoBoton);
-    //Con el click se dispara un evento para poder eliminar el padre del elemente del boton que seria el ul.
-    boton.addEventListener('click', (e) => {
-        const item = e.target.parentElement;
-        ul.removeChild(item);
-    });
-
-
-    //Ahora vamos a interta que se pueda editar la tarea.
-    const botonEditar = document.createElement('button');
-    botonEditar.textContent = "Editar tarea."
-    li.appendChild(botonEditar);
-    botonEditar.addEventListener('click', (e) => {
-        const editarTarea = prompt("Editar la tarea: ");
-        e.target.parentElement.textContent = editarTarea;
-    });
-
-
+const ul = document.getElementById('ul');
+const botonAgregarTarea = document.getElementById('listarTarea');
+botonAgregarTarea.addEventListener('click', () => {
+    console.log("aprete el boton de listar");
+    const element = `
+                    <li>
+                        <p>${input.value}</p>
+                        <button type="submit" id="0">eliminar</button>
+                        <button type="submit" id="0">editar</button>
+                    </li>
+                    `
+    ul.insertAdjacentHTML("beforeend", element);
     input.value = "";
-}
+})
 
+
+
+/*
+<li>
+            <p></p>
+            <button type="submit" id="0">eliminar</button>
+            <button type="submit" id="0">editar</button>
+        </li>
+
+*/
 
